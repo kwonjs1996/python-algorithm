e451b0f783aab7640bcb53d2fd96da51df4a2d0f
kwonjs1996 <kwonjs1996@gmail.com>
2022-08-11 20:38:23 +0900

    역수열(그리디)

---
+++ inflearn/역수열(그리디).py
@@ -0,0 +1,12 @@
+n = int(input())
+a = list(map(int, input().split()))
+a.insert(0, 0)
+seq = [0]*n
+for i in range(1, n+1):
+    for j in range(n):
+        if a[i] == 0 and seq[j] == 0:
+            seq[j] = i
+        elif seq[j] == 0:
+            a[i] -= 1
+for x in seq:
+    print(x, end='')
